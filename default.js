e535fd54eb422afe21404e4f4cd90173349c500d
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-09-29 15:32:08 +0530

    added default parameter

---
+++ default.js
@@ -0,0 +1,6 @@
+function shopping(cumb= 40){
+    const total = cumb;
+    return total;
+}
+const weekendShopping = shopping();
+console.log(weekendShopping);
---
+++ v-3 let-const.js
@@ -0,0 +1,2 @@
+// {in here used ONly "let" and "const" instead of  "var" as a variable}
+// {in here used ONly "let" and "const" instead of  "var" }
